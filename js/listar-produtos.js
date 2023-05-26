8afe28b971acaca8b154050d0afdcb3c821569a0
Felipe Vaccarini <you@example.com>
2023-05-26 15:24:25 -0300

    Remover Produto

--- js/listar-produtos.js
+++ js/listar-produtos.js
@@ -1,6 +1,6 @@
 $(document).ready(listarProdutos);
 
- function listarProdutos() {
+function listarProdutos() {
 
     $.ajax({
         url: 'http://localhost:8080/api/produto/list',
@@ -10,18 +10,35 @@ $(document).ready(listarProdutos);
             console.log(result);
             var html = '';
             $.each(result, function (i, data) {
-                 html += `<tr><td>` + data.id + `</td>`;
-                 html += `<td>` + data.nome + `</td>`;
-                 html += `<td><a href="editarProduto.html?id=` + data.id + `"><i class="bi bi-pencil-fill"></i></a>`;
-                 html += ` <a href="visualizarProduto.html?id=` + data.id + `"><i class="bi bi-search"></i></a>`;
-                 html += ` <a href="#" onclick="removerProduto(` + data.id + `)"><i class="bi bi-archive-fill"></i></a></td></tr>`;
+                html += `<tr><td>` + data.id + `</td>`;
+                html += `<td>` + data.nome + `</td>`;
+                html += `<td><a href="editarProduto.html?id=` + data.id + `"><i class="bi bi-pencil-fill"></i></a>`;
+                html += ` <a href="visualizarProduto.html?id=` + data.id + `"><i class="bi bi-search"></i></a>`;
+                html += ` <a href="#" onclick="removerProduto(` + data.id + `)"><i class="bi bi-archive-fill"></i></a></td></tr>`;
 
-                 $("#tbListarProdutosBody").html(html);
-             });
+                $("#tbListarProdutosBody").html(html);
+            });
 
-             let table = new DataTable('#tbListarProdutos');
+            let table = new DataTable('#tbListarProdutos');
         }
     })
+}
+function removerProduto(id) {
 
+    var respostaPergunta = confirm("Confirma a exclusão?");
+    if (respostaPergunta == true) {
 
- }
+        $.ajax({
+            type: 'DELETE',
+            url: 'http://localhost:8080/api/produto/remove/' + id,
+            dataType: 'json',
+            success: function (result) {
+                location.reload();
+            },
+            error: function (result) {
+                alert(result);
+            }
+        })
+
+    }
+}
