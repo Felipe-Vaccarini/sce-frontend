f3d7c8366b89b8b9420804c18e004e2d1e3a1238
Felipe Vaccarini <you@example.com>
2023-06-21 15:12:38 -0300

    adicionado marca, data de fabricação, preço no listar produtos

--- js/listar-produtos.js
+++ js/listar-produtos.js
@@ -12,12 +12,18 @@ function listarProdutos() {
             $.each(result, function (i, data) {
                 html += `<tr><td>` + data.id + `</td>`;
                 html += `<td>` + data.nome + `</td>`;
+                html += `<td>` + data.marca + `</td>`;
+                var fabricacaoDate = new Date(data.fabricacao);
+                fabricacaoDate.setDate(fabricacaoDate.getDate() + 1);
+                html += `<td>` + fabricacaoDate.toLocaleDateString() + `</td>`; 
+                html += `<td>` + 'R$' + data.preco.toFixed(2) + `</td>`;              
                 html += `<td><a href="editar-produto.html?id=` + data.id + `"><i class="bi bi-pencil-fill"></i></a>`;
                 html += ` <a href="visualizar-produto.html?id=` + data.id + `"><i class="bi bi-search"></i></a>`;
                 html += ` <a href="#" onclick="removerProduto(` + data.id + `)"><i class="bi bi-archive-fill"></i></a></td></tr>`;
-
-                $("#tbListarProdutosBody").html(html);
             });
+            
+            $("#tbListarProdutosBody").html(html);
+            
 
             let table = new DataTable('#tbListarProdutos', {
                 language:{
